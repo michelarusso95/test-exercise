f5f90e4a183307f1b6f898370c3c68f6235a1720
Michela Russo <49483343+michelarusso95@users.noreply.github.com>
2021-05-14 15:53:53 +0200

    Add files via upload

---
+++ src/test/java/exercise/testexercise/TestExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.testexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
