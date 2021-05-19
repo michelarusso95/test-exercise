caf7e16bbd1146e039afb7dc71793de4b312d112
Michela Russo <mrusso@red.software.systems>
2021-05-19 13:54:34 +0200

    api-docs

---
+++ src/main/java/exercise/testexercise/config/SpringFoxConfig.java
@@ -0,0 +1,20 @@
+package exercise.testexercise.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("exercise.testexercise.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
